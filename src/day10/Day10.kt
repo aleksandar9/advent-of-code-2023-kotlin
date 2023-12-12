cacd6d4282b8cb3f88fd0fe09db1506da620fb7f
Aleksandar Ninevski <aleksandar.nine@gmail.com>
2023-12-12 19:42:17 +0100

    Day 10 Part 2 still WIP

--- src/day10/Day10.kt
+++ src/day10/Day10.kt
@@ -76,14 +76,78 @@ fun main() {
         return grid.maxOf { it.distance ?: 0 }
     }
 
+    fun Node.isInsidePolygon(grid: List<Node>) {
+        val left = grid.filter { it.x < this.x && it.y == this.y && it.distance != null }
+        val leftEmpty = grid.filter { it.x < this.x && it.y == this.y && it.distance == null }
+        val right = grid.filter { it.x > this.x && it.y == this.y && it.distance != null }
+        val rightEmpty = grid.filter { it.x > this.x && it.y == this.y && it.distance == null }
+        val up = grid.filter { it.x == this.x && it.y < this.y && it.distance != null }
+        val upEmpty = grid.filter { it.x == this.x && it.y < this.y && it.distance == null }
+        val down = grid.filter { it.x == this.x && it.y > this.y && it.distance != null }
+        val downEmpty = grid.filter { it.x == this.x && it.y > this.y && it.distance == null }
+        if (left.count() % 2 == 1 && right.count() % 2 == 1 && up.count() % 2 == 1 && down.count() % 2 == 1) {
+            this.value = '*'
+        } else if (value == '.' && left.count() > 0 && right.count() > 0 && up.count() > 0 && down.count() > 0) {
+            this.value = 'O'
+        }
+    }
+
     fun part2(input: List<String>): Int {
-        return 0
+        val grid = mutableListOf<Node>()
+        input.forEachIndexed { y, string ->
+            string.forEachIndexed { x, char ->
+                grid.add(Node(x, y, char))
+            }
+        }
+
+        grid.forEach { node ->
+            if (node.value != '.' && node.value != 'I') {
+                node.adjacentNodes.addAll(
+                    node.adjacentNodes(grid)
+                )
+            }
+        }
+
+        val deque = ArrayDeque<Node>()
+        val start = grid.find { it.value == 'S' }!!
+        start.distance = 0
+        deque.addFirst(start)
+
+        while (deque.isNotEmpty()) {
+            val current = deque.removeFirst()
+            current.adjacentNodes.forEach {
+                if (it.distance == null) {
+                    it.distance = current.distance!! + 1
+                    deque.addLast(it)
+                }
+            }
+        }
+
+        grid.forEach {
+            if (it.distance == null) {
+                it.isInsidePolygon(grid)
+            }
+        }
+
+        for (y in 0 until input.size) {
+            for (x in 0 until input[0].count()) {
+                val node = grid.find { it.x == x && it.y == y }
+                print(node!!.value)
+            }
+            println()
+        }
+
+        println("(*) = ${grid.count { it.value == '*' }}")
+        println("(O) = ${grid.count { it.value == 'O' }}")
+
+        return grid.count { it.value == '*' }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput(pkg = "day10", name = "Day10_test")
+    val testInput2 = readInput(pkg = "day10", name = "Day10_test2")
     check(part1(testInput) == 8)
-    check(part2(testInput) == 0)
+    check(part2(testInput2) == 1)
 
     val input = readInput(pkg = "day10", name = "Day10")
     println(part1(input))
